9455edb8b8c9004000d9410b86458bd6f4aa108b
Andrew Eames <ajerxs@gmail.com>
2020-11-08 22:53:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,9 @@ def take_a_number(katz_deli, person)
   katz_deli << person
   puts "Welcome, #{person}. You are number #{katz_deli.size} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  end
+end
