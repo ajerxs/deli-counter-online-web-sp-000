7c2013ebf213cc632a2866269829fd5b1771ce48
Andrew Eames <ajerxs@gmail.com>
2020-11-08 22:04:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{katz_deli}."
+    katz_deli.each do |list|
+    puts "The line is currently: #{list}."
   end
 end
